f5d4e26cfec5f4246bb4b8dd78dcfaacca1c0489
Aniccha <shubhwaghmare27@gmail.com>
2024-09-14 18:18:04 +0530

    Rest API done

--- backend/controller/bookController.js
+++ backend/controller/bookController.js
@@ -5,7 +5,7 @@ try {
     let data=req.body
     if (data.Tital&&data.Auther&&data.Publish_year)
     {
-    let isUser= await bookModel.findOne(data);
+    let isUser= await bookModel.findOne({Tital:data.Tital});
     if(isUser){
         return res.status(400).send({message:"Book already exists"});
     }
@@ -20,7 +20,70 @@ try {
     }
 } catch (error) {
     console.log("internal error "+error.message);
+    res.status(400).send({error:"inernal server error"+error.message})
+
+}
+}
+//get all books
+const getAll=async (req,res)=>{
+    try {
+        const books=await bookModel.find();
+        return res.status(201).send({allBooks:books});
+        
+    } catch (error) {
+        console.log("internal error "+error.message);
+        res.status(400).send({error:"inernal server error"+error.message})
+    }
+}
+//get by id
+const getById=async (req,res)=>{
+try {
+    const {id}=req.params;
+    const book=await bookModel.findById(id);
+    if(book) return res.status(201).send(book);
+    else return res.status(400).send({message:"book dosent exist"});
+
+} catch (error) {
+    res.status(400).send({error:"inernal server error"+error.message})
+}
+}
+// update data
+const updateData=async (req,res)=>{
+try {
+    console.log(req.query);
     
+    if(req.query){
+        const book=await bookModel.findOne(req.query);
+        if(book){
+            const update=await bookModel.updateOne({...req.query},{$set:{...req.body}});
+            return res.status(201).send({message:"data updated successfully"})
+        }
+        else{
+            res.status(400).send({message:"user not found."})
+        }
+    }
+    else{
+        res.status(400).send({message:"provide details"});
+    }
+} catch (error) {
+    res.status(400).send({error:"internal Server error"+error.message})
 }
 }
-module.exports={Register}
+/// delete data
+const deleteData=async (req,res)=>{
+ try {
+    const{id}=req.params;
+    const response=await bookModel.findByIdAndDelete(id);
+    if(response)
+    {
+        return res.status(200).send({ message: "book detail deleted" })
+    }
+    else{
+        return res.status(404).send({ error: "user not found" })
+    }
+ } catch (error) {
+    return res.status(500).send({ error: "Internal Server Error", msg: error.message })
+
+ }
+}
+module.exports={Register,getAll,getById,updateData,deleteData}
--- backend/routes/bookRoute.js
+++ backend/routes/bookRoute.js
@@ -1,7 +1,16 @@
 const express = require("express");
-const { Register } = require("../controller/bookController");
+const { Register, getAll, getById, updateData, deleteData } = require("../controller/bookController");
 const bookRoute=express.Router();
+
+
 //enter books
 bookRoute.post("/register",Register);
-
+//get all data
+bookRoute.get("/getAll",getAll);
+//get bt id
+bookRoute.get("/get/:id",getById)
+//update data
+bookRoute.put("/update",updateData)
+//delete data
+bookRoute.delete("/delete/:id",deleteData);
 module.exports=bookRoute;
